dc2e05ffcfc245afa86de2d15b5f070df27b8181
cw1753 <chunwu0186@gmail.com>
2022-03-27 19:18:41 -0400

    Unfinish and sample testing code

---
+++ app.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+// const app = require('./index');
+
+// TODO Add more test. Not enough time.
+describe('REST API', () => {
+    it('GET /api/truck', () => {
+        // return request(app).get('/api/truck').expect(200)
+    })
+})
